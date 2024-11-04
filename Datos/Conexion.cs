0a24e33fffc7397a22ae2f579fe6fd6b6827e993
Carlos Garay Muñoz <carloxgaray@outlook.com>
2024-11-04 16:57:01 -0300

    se actualiza la clase conexion

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -1,10 +1,4 @@
 ﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Datos
 {
@@ -46,8 +40,7 @@ namespace Datos
                 if (ConnectionMySql.State == System.Data.ConnectionState.Closed)
                 {
                     MySqlConnection.ClearAllPools();
-                    //ConnectionMySql = new MySqlConnection("server=192.168.0.7; database=caja_tbk; Uid=root; pwd=apoca11; SslMode=none; Pooling=false;");
-                    ConnectionMySql = new MySqlConnection("server=ec2-18-223-30-246.us-east-2.compute.amazonaws.com; database=caja_tbk; Uid=root; pwd=Smith3976!; SslMode=none; Pooling=false;");
+                   ConnectionMySql = new MySqlConnection("server=ec2-18-223-30-246.us-east-2.compute.amazonaws.com; database=caja_tbk; Uid=root; pwd=Smith3976!; SslMode=none; Pooling=false;");
                     ConnectionMySql.Open();
                     return ConnectionMySql;
                 }
@@ -56,7 +49,6 @@ namespace Datos
                     CerrarConnectionMysql();
 
                     MySqlConnection.ClearAllPools();
-                    //ConnectionMySql = new MySqlConnection("server=192.168.0.7; database=caja_tbk; Uid=root; pwd=apoca11; SslMode=none; Pooling=false;");
                     ConnectionMySql = new MySqlConnection("server=ec2-18-223-30-246.us-east-2.compute.amazonaws.com; database=caja_tbk; Uid=root; pwd=Smith3976!; SslMode=none; Pooling=false;");
                     ConnectionMySql.Open();
                     return ConnectionMySql;
@@ -78,7 +70,6 @@ namespace Datos
                 }
 
                 MySqlConnection.ClearAllPools();
-                //ConnectionMySql = new MySqlConnection("server=192.168.0.7; database=sige_sam_V3; Uid=root; pwd=apoca11; SslMode=none; Pooling=false;");
                 ConnectionMySql = new MySqlConnection("server=ec2-18-223-30-246.us-east-2.compute.amazonaws.com; database=sam_diagnostico; Uid=root; pwd=Smith3976!; SslMode=none; Pooling=false;");
                 ConnectionMySql.Open();
                 return ConnectionMySql;
