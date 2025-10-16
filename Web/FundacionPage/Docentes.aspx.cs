bdd981a682afc42ed572dbc7651a2b3ecadacd99
Carlos Garay Muñoz <carloxgaray@outlook.com>
2025-10-16 18:52:29 -0300

    Se agrega el envio de contacto

--- Web/FundacionPage/Docentes.aspx.cs
+++ Web/FundacionPage/Docentes.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,12 +20,36 @@ namespace Web.FundacionPage
 
         protected void LinkButtonEnviar_Click(object sender, EventArgs e)
         {
+            Mails mails = new Mails();
 
+            string nombre = TextBoxNombre.Text + " " + TextBoxApellido.Text;
+            string Insti = TextBoxInstituto.Text;
+            string Correo = TextBoxMail.Text;
+            string fono = TextBoxFono.Text;
+            string programa = "Capacitaciones Docentes";
+            string mensaje = TextAreaMensaje.Value;
+
+            string resultado = mails.SendEmailFundacion(nombre, Insti, Correo, fono, programa, mensaje);
+
+            if (resultado == "ok")
+            { // 
+                error_icono.Visible = false;
+                ok_icono.Visible = true;
+                LabelMensajeError.Text = "Gracias por contactarnos. En breve recibirás una respuesta.";
+                ClientScript.RegisterStartupScript(GetType(), "Popup", string.Format("ShowModalMensaje();"), true);
+            }
+            else
+            {
+                error_icono.Visible = true;
+                ok_icono.Visible = false;
+                LabelMensajeError.Text = "Error al generar el contacto";
+                ClientScript.RegisterStartupScript(GetType(), "Popup", string.Format("ShowModalMensaje();"), true);
+            }
         }
 
         protected void LinkButtonCargarPagina_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect(Request.Url.AbsoluteUri);
         }
     }
 }
